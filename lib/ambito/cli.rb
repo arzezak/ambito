31f35f8a3cf7b616abda42a7b499475dffeb1209
Ariel Rzezak <arzezak@gmail.com>
2023-11-22 17:41:33 -0300

    CLI accepts arguments

--- lib/ambito/cli.rb
+++ lib/ambito/cli.rb
@@ -1,7 +1,25 @@
+require "optparse"
+
 module Ambito
   class CLI
+    def initialize(argv)
+      @argv = argv
+    end
+
     def run
-      puts Ambito.run
+      puts options
+    end
+
+    private
+
+    def options
+      {}.tap do |parsed_options|
+        OptionParser.new do |options|
+          options.on("-d", "--dollar DOLLAR", "Select the rate") do |dollar|
+            parsed_options[:dollar] = dollar
+          end
+        end.parse(@argv)
+      end
     end
   end
 end
