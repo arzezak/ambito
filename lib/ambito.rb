233f52d828ae361a99a340f8c6268f040fd4006e
Ariel Rzezak <arzezak@gmail.com>
2023-11-22 09:53:34 -0300

    Initialization

---
+++ lib/ambito.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ambito/version"
+
+module Ambito
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ambito/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ambito
+  VERSION = "0.1.0"
+end
